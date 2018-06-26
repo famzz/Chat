4a5838d9f97543648889cf0614b75764f4e45a64
Abdul Al-Fahim <4104094+famzz@users.noreply.github.com>
2018-06-26 21:31:52 +0100

    Established communication! Successfully got the server to actively accept new clients and managed to pass one message between them. Need to figure a non-blocking implementation of the client's input-recv model.

--- Client.py
+++ Client.py
@@ -2,11 +2,30 @@ import socket
 
 
 host = "localhost"
-port = 50007
+port = 5005
 
-message = input("Please enter message to send:\n")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+sock.setblocking(0)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((host, port))
+def send_message(username, message):
     message += "EOD"
+    message = username + ":" + message
     sock.send(message.encode("utf-8"))
+
+
+username = input("Please enter your username:\n")
+
+recipient = input("Please enter the user that you would like to talk to:\n")
+
+send_message(username, "recipient" + recipient)
+
+while True:
+    try:
+        message = sock.recv(1024)
+        print(message.decode("utf-8"))
+    except:
+        pass
+
+    chat = input("Please enter message to send to: " + recipient + ":\n")
+    send_message(username, chat)
--- Server.py
+++ Server.py
@@ -1,23 +1,85 @@
 import socket
+from threading import *
+
+ip = '127.0.0.1'
+port = 5005
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ip, port))
+
+conversations = {}
+conversation_lock = Lock()
 
 
-host = "localhost"
-port = 50007
+def add_conversation(username, recipient):
+    conversation_lock.acquire()
+    try:
+        conversations[username] = recipient
+    finally:
+        conversation_lock.release()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((host, port))
-    sock.listen(1)
-    conn, addr = sock.accept()
-    with conn:
-        print('Connected by ' + addr[0])
-        message = ""
-        while True:
-            chunk = conn.recv(1024)
 
-            message += chunk.decode("utf-8")
+def get_recipient(username):
+    conversation_lock.acquire()
+    try:
+        return conversations.get(username)
+    finally:
+        conversation_lock.release()
 
-            if message[-3:] == "EOD":
-                message = message[:-3]
-                break
 
-print("Message received was:\n" + message)
+client_sockets = {}
+client_socket_lock = Lock()
+
+
+def add_client_socket(username, socket):
+    client_socket_lock.acquire()
+    try:
+        client_sockets[username] = socket
+    finally:
+        client_socket_lock.release()
+
+
+def get_client_socket(username):
+    client_socket_lock.acquire()
+    try:
+        return client_sockets[username]
+    finally:
+        client_socket_lock.release()
+
+
+class Client(Thread):
+    def __init__(self, sock, addr):
+        Thread.__init__(self)
+        self.sock = sock
+        self.addr = addr
+        self.message = ""
+        self.start()
+
+    def run(self):
+        while 1:
+            chunk = self.sock.recv(1024).decode("utf-8")
+            self.message += chunk
+
+            if self.message[-3:] == "EOD":
+                self.message = self.message[:-3]
+
+                username, message = self.message.split(":", 1)
+                if "recipient" in message:
+                    recipient = message.split("recipient", 1)[1]
+                    add_conversation(username, recipient)
+                    add_client_socket(username, self.sock)
+                else:
+                    recipient = get_recipient(username)
+                    recipient_socket = get_client_socket(recipient)
+                    recipient_socket.send(self.message.encode("utf-8"))
+
+                self.message = ""
+
+
+s.listen(2)
+
+
+while 1:
+    clientsocket, address = s.accept()
+
+    Client(clientsocket, address)

43dd9184aa97a342f267cdc75a0840bc30779ba9
Abdul Al-Fahim <4104094+famzz@users.noreply.github.com>
2018-07-14 00:07:21 +0100

    Rename server.py module to Server.py module to better follow current conventions. Moved ServerMain.py module to root of project to avoid ModuleNotFoundErrors.

--- ServerMain.py
+++ ServerMain.py
@@ -1,10 +1,10 @@
-import Server.server
+import Server.Server
 import tkinter as tk
 import Server.GUI.ServerWindow
 
 
 if "__main__" in __name__:
-    server = Server.server.Server()
+    server = Server.Server.Server()
 
     root = tk.Tk()
     Server.GUI.ServerWindow.ServerWindow(root, server)
